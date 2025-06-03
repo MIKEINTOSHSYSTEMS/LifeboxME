af7d02c15854fc998e71735d8a0bd32c191d9d61
MIKEINTOSHSYSTEMS <mikeintoshsys@gmail.com>
2025-06-03 20:33:53 +0300

    Added meta tags for description, keywords, and author in index.php

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lifebox M&E System</title>
+    <meta name="description" content="Welcome to Lifebox M&E System Dashboard, Data Exploration and Administration System">
+    <meta name="keywords"
+        content="Monitoring, Evaluation, Lifebox, Surgical, Reporting, Dashboard, and, Data, Data Management, Exploration, Portal, MIKEINTOSH, Systems, System, MERQ Consultancy">
+    <meta name="author" content="MERQ Consultancy">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
