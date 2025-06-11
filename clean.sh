e4d140e7364bcf35a0c8abd498f5e6675735cf9a
MIKEINTOSHSYSTEMS <mikeintoshsys@gmail.com>
2025-06-11 22:04:40 +0300

    Add sudo to cleanup commands in clean.sh for permission handling

--- clean.sh
+++ clean.sh
@@ -9,9 +9,13 @@ docker-compose -p lifebox -f dev.docker-compose.yml --env-file $ENV_FILE down --
 
 # Clean data directories
 echo "Cleaning all data directories..."
-rm -rf ./data/pgadmin-data
-rm -rf ./data/postgres-data
-rm -rf ./data/postgres-init
+sudo rm -rf ./data/pgadmin-data ./data/postgres-data ./data/postgres-init
+
+git stash
+
+git pull
+
+chmod +x start.sh
 
 echo "Full cleanup completed. Running containers:"
 docker-compose -p lifebox -f dev.docker-compose.yml ps
