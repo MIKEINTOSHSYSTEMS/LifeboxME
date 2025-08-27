ffc5039946afb95ea1fe63b85638718796299568
MIKEINTOSHSYSTEMS <mikeintoshsys@gmail.com>
2025-08-28 00:25:11 +0300

    updated base forms url params for prod

--- modules/forms/config/db.php
+++ modules/forms/config/db.php
@@ -6,8 +6,8 @@ return [
     'schemaCacheDuration' => 86400,
     'schemaCache' => 'cache',
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=lifeboxme_forms', // dev
-//    'dsn' => 'mysql:host=192.168.47.5;port=3306;dbname=lifeboxme_forms', // prod
+//    'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=lifeboxme_forms', // dev
+    'dsn' => 'mysql:host=192.168.47.5;port=3306;dbname=lifeboxme_forms', // prod
     'username' => 'lifeboxme_system',
     'password' => 'lifeboxme_system',
 ];
